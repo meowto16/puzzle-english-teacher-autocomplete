ed7eb559ebee60652af4d4d9e718d3f15c383ec9
Maxim Kirshin <mak@crtweb.ru>
2021-09-27 03:39:36 +0500

    feat: select-text-test helper added

--- src/js/teacher/content_scripts/helpers/puzzle-english.helper.ts
+++ src/js/teacher/content_scripts/helpers/puzzle-english.helper.ts
@@ -1,16 +1,19 @@
 import PuzzlesTestHelper from './puzzles-test.helper'
 import PuzzleEnglishBaseHelper from './puzzle-english-base.helper'
+import SelectTextTestHelper from './Select-Text-Test.helper'
 
-export type PuzzleEnglishHelperSelector = 'puzzles_test'
+export type PuzzleEnglishHelperSelector = 'puzzles_test' | 'select_text_test'
 
 class PuzzleEnglishHelper {
   public static currentHelper: PuzzleEnglishBaseHelper
   private static selectorToHelperMap: Record<PuzzleEnglishHelperSelector, typeof PuzzleEnglishBaseHelper> = {
-    'puzzles_test': PuzzlesTestHelper
+    'puzzles_test': PuzzlesTestHelper,
+    'select_text_test': SelectTextTestHelper,
   }
 
   static defineHelperClass($item: JQuery): PuzzleEnglishHelperSelector {
     if ($item.hasClass('puzzles_test')) return 'puzzles_test'
+    if ($item.hasClass('select_text_test')) return 'select_text_test'
   }
 
   static defineCurrentHelper(selector: string) {
